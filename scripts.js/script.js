2de70ce0a0e540a5ec6f5e217e657d4e488daa5d
Marcio <marcioabcd11@gmail.com>
2021-07-28 13:05:57 -0300

    fliping Card With Random Gif

--- scripts.js/script.js
+++ scripts.js/script.js
@@ -1,5 +1,8 @@
 let numeroCartas = 0;
-jogo = document.querySelector('.jogo');
+let listaGifs = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'].sort(comparador);
+const jogo = document.querySelector('.jogo');
+let listaGifsFinal = [];
+
 
 function validar(){
     let cartasSuficientes = Boolean(numeroCartas >= 4 && numeroCartas <= 14);
@@ -19,21 +22,36 @@ function criarJogo(){
     let lista = '';
 
     while(contador < numeroCartas){
-        lista += ('<li class="carta" onclick="mudarCarta(this);"> <img src="assets/front.png" alt="Carta do jogo"> </li>')
+        lista += ('<li name="' + contador + '" class="carta" onclick="mudarCarta(this);"> <img src="assets/front.png " alt="Carta do jogo"> </li>')
 
         contador += 1;
     }
+
+    contador = 0;
+
+    while(contador < numeroCartas/2){
+        let contadorAux = 0;
+
+        while(contadorAux < 2){
+            listaGifsFinal.push(listaGifs[contador]);
+            contadorAux += 1
+        }
+
+        contador += 1;
+    }
+
+    listaGifsFinal = listaGifsFinal.sort(comparador);
 
     jogo.innerHTML = lista;
     jogo.classList.remove('sumir');
 }
 
+function comparador() {
+	return Math.random() - 0.5;
+}
 
-
-/* minhaArray.sort(comparador);
-
-function comparador() { 
-	return Math.random() - 0.5; 
-} */
+function mudarCarta(elemento){
+    elemento.innerHTML = '<img src="assets/'+ listaGifsFinal[elemento.getAttribute('name')] +'" alt="Carta do jogo">';
+}
 
 validar();
