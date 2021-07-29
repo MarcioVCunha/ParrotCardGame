949828beb7dae5e917fd01834abfd115745bed2e
Marcio <marcioabcd11@gmail.com>
2021-07-29 08:51:41 -0300

    flip not working, why?

--- scripts.js/script.js
+++ scripts.js/script.js
@@ -1,49 +1,42 @@
 let numeroCartas = 0;
+
+//Pegando os gifs em ordem aleatória
 let listaGifs = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'].sort(comparador);
+
 const jogo = document.querySelector('.jogo');
-let listaGifsFinal = [];
 
 
 function validar(){
     let cartasSuficientes = Boolean(numeroCartas >= 4 && numeroCartas <= 14);
     let par = Boolean(numeroCartas%2 === 0);
 
-    if(!(cartasSuficientes && par)){
-        numeroCartas = prompt("Com quantas cartas deseja jogar? (Entre 4 e 14 e deve ser par)");
+    if(cartasSuficientes && par){
+        criarJogo();
+    } else {
+        numeroCartas = Number(prompt("Com quantas cartas deseja jogar? (Entre 4 e 14 e deve ser par)"));
 
         validar();
-    } else {
-        criarJogo();
     }
 }
 
 function criarJogo(){
-    let contador = 0;
-    let lista = '';
+    let listaGifsFinal = [];
 
-    while(contador < numeroCartas){
-        lista += ('<li name="' + contador + '" class="carta" onclick="mudarCarta(this);"> <img src="assets/front.png " alt="Carta do jogo"> </li>')
-
-        contador += 1;
-    }
-
-    contador = 0;
-
-    while(contador < numeroCartas/2){
-        let contadorAux = 0;
-
-        while(contadorAux < 2){
-            listaGifsFinal.push(listaGifs[contador]);
-            contadorAux += 1
+    for(let i = 0; i < numeroCartas/2; i += 1){
+        for(let j = 0; j < 2; j += 1){
+            listaGifsFinal.push(listaGifs[i]);
         }
-
-        contador += 1;
     }
 
     listaGifsFinal = listaGifsFinal.sort(comparador);
 
+    let lista = '';
+
+    for(let i = 0; i < numeroCartas; i += 1){
+        lista += ('<div class="carta"> <div class="front-face face" onclick="mudarCarta(this);"> <img src="assets/front.png" alt="Carta do jogo"> </div> <div class="back-face face"> <img src="assets/bobrossparrot.gif"> </div> </div>')
+    }
+
     jogo.innerHTML = lista;
-    jogo.classList.remove('sumir');
 }
 
 function comparador() {
@@ -51,7 +44,7 @@ function comparador() {
 }
 
 function mudarCarta(elemento){
-    elemento.innerHTML = '<img src="assets/'+ listaGifsFinal[elemento.getAttribute('name')] +'" alt="Carta do jogo">';
+    elemento.classList.add("flipCard");
 }
 
 validar();
